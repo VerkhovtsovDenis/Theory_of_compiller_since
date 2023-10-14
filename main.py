65b1008510a2dece62f371e526dc78e6cbab6d40
VerkhovtsovDenis <verkhovtcov.do@dvfu.ru>
2023-10-14 16:36:29 +1000

    add print info

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import unittest
 from LanguageHandTest import LanguageHandTest
+from Language import Language
 
 LanguageHandTest()
 
+def main():
+    l1, l2, _ = LanguageHandTest.readfile()
+
+    # result - результат выполнения конкатенации программой
+    result = (Language(value=l1) * Language(value=l2)).value
+
+    print(result)
+
 if __name__ == '__main__':
-    unittest.main()
+    main()
+    #unittest.main()
+
+
